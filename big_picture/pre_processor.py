8751d53a393da49e4a0e2e9101db897e7ce11015
JoÃao Martins <joaoambiente@gmail.com>
2021-06-10 11:55:27 +0100

    Inclui geracao de ficheiro merge dos 30k csvs para preprocessing

--- big_picture/pre_processor.py
+++ big_picture/pre_processor.py
@@ -14,7 +14,7 @@ import pandas as pd
 
 # Arguments to change based on data location and filename
 # Data for loading .csv
-REL_PATH_INPUT = "../raw_data/data_12k/"
+REL_PATH_INPUT = "../raw_data/data_30k/"
 
 # Relevant Columns in .csv to change based on data source
 CONTENT_COL = "content"
@@ -42,21 +42,21 @@ def pre_process(df,sample=None,
 
     df[CONTENT_COL] = df[CONTENT_COL].replace('\n',' ', regex=True)
 
-    # print("read_csv")
+    print("read_csv")
     
     # Drop NA's and drop columns where there's only the string "Invalid file"
     df[news_all_data] = df[CONTENT_COL] + " " + df[DESCRIPTION_COL] + " " + df[HEADLINE_COL]
     df = df.dropna(subset=[news_all_data]).reset_index()
     df = df[df[news_all_data] != "Invalid file"].reset_index(drop=True)
 
-    # print("read_csv")
+    print("read_csv")
 
     #import ipdb; ipdb.set_trace()
     # 1.1 lowercase "news + headline" column
     if lower_case or all_true: 
         df[news_all_data] = df[news_all_data].str.lower()
 
-    # print("lowercase")
+    print("lowercase")
 
     # 1.2 Create number of "news + headline" decimals column (we need to find a way to use this in the future)
     df['nrs_count'] = df[news_all_data].str.count('\d')
@@ -97,7 +97,7 @@ def pre_process(df,sample=None,
         df[news_all_data] = df[news_all_data]\
                                 .apply(lambda x: [word for word in x if not word in stop_words])
 
-        # print("stopwords")
+        print("stopwords")
 
     # 1.8a Stemming (optional)
     if stem_not_lematize:
@@ -106,7 +106,7 @@ def pre_process(df,sample=None,
         df[news_all_data] = df[news_all_data]\
                                     .apply(lambda x: [stemmer.stem(word) for word in x])
 
-        # print("stemming")
+        print("stemming")
 
     # 1.8b Lematizing with POS tags in english (optional)
     if lematize or (all_true and not stem_not_lematize):
@@ -125,7 +125,7 @@ def pre_process(df,sample=None,
         df[news_all_data] = df[news_all_data]\
                                     .map(lambda x: [lemmatizer.lemmatize(word, get_wordnet_pos(word)) for word in x])
 
-        # print("lematizing")
+        print("lematizing")
 
    # 1.9 Adding vocab richness column (we need to find a way to use this column in the future)
     def vocab_richness(text):
@@ -139,9 +139,24 @@ def pre_process(df,sample=None,
     df[news_all_data] = df[news_all_data].map(lambda x: ' '.join(x))
     df['vocab richness'] = df[news_all_data].apply(lambda x: vocab_richness(x))
 
-    # print("vocab_richness")
+    print("vocab_richness")
 
     return df
 
 if __name__ == "__main__":
-    print(pre_process(**args_methods))
+    from big_picture.get_merged_data import get_data
+
+    df =  get_data(REL_PATH_INPUT)
+    
+    df1 = pre_process(df,
+                all_true=True, 
+                lower_case=False, 
+                no_digits=False,
+                rem_punct=False,
+                rem_stopwords=False,
+                stem_not_lematize=False,
+                lematize=False)
+
+    print(df1.news_all_data.head(5))
+
+    df1.to_csv(r'/data/data_30k_all_true.csv', index = False, header=True)
